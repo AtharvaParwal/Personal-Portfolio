38928e1daf6cf1e44e268540ae85901a20512375
AtharvaParwal <atharvaparwal0608@gmail.com>
2024-09-23 22:04:21 +0530

    Resume Updated

--- src/data/UserData.jsx
+++ src/data/UserData.jsx
@@ -1,7 +1,7 @@
 export const UserData = {
   name: "Atharva Parwal",
   about : "I am currently in my third year of pursuing B.Tech in Computer Science Engineering from NIT Kurukshetra. I have a strong command of programming languages such as Java, C, and Python, as well as a solid understanding of MySQL. My proficiency extends to web development, where I excel in creating dynamic and responsive websites using HTML, CSS, JavaScript, and React.js. Additionally, I am adept at building robust back-end applications with Node.js, Express.js, and MongoDB. I am passionate about developing efficient and high-quality solutions that meet and exceed expectations.",
-  resumeUrl : "https://drive.google.com/file/d/1q97BHyAdR0jWh9X8f6VFLpK9iny_BLAS/view?usp=sharing",
+  resumeUrl : "https://drive.google.com/file/d/1JECYZAp2Y1uyZEb3uvJeSABWmvORvNP1/view?usp=sharing",
   FooterLink : "mailto:atharvaparwal0608@gmail.com",
   socialMedia: [
     {
